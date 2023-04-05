0a43a9c4a9dec6c9d6c28e74acb9622bd48416b2
mohdrubaid <125140108+mohdrubaid@users.noreply.github.com>
2023-04-05 16:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort(function (a,b){return a -b});
+	let closestSum = Infinity;
+	for(let i=0;i<arr.length-2;i++){
+		let left=i+1;
+		let right=arr.length-1;
+		while(left<right){
+			let sum=arr[i]+arr[left]+arr[right];
+			if(Math.abs(target-sum)<Math.abs(target-closestSum)){
+				closestSum=sum;
+			}
+			if(sum<target){
+				left++;
+			}
+			else{
+				right--
+			}
+		}
+	}
+	return closestSum;
   
 }
 
